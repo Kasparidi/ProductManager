7ff403e21728803905cb2271e20e2c38220df8a1
Kasparidi <kasparidi@gmail.com>
2020-10-29 15:17:03 +0300

    rework second condition

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -13,22 +13,7 @@ public class Product {
     private String name;
     private int price;
 
-    private ProductRepository repository;
-    public Product (ProductRepository repository) {
-        this.repository = repository;
-    }
-
-    public boolean matches(String text) {
-        Product[] result = new Product[0];
-        for (Product product: repository.findAll()) {
-            if (product.matches(text)) {
-                Product[] tmp = new Product[result.length + 1];
-                System.arraycopy(result, 0, tmp, 0, result.length);
-                tmp[tmp.length - 1] = product;
-                result = tmp;
-            }
-            return true;
-        }
-        return false;
+    public boolean matches(String search) {
+        return matches(search);
     }
 }
